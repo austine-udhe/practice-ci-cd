013de046ccc4c0e5da5eccaaafa8328ff2294618
austine-udhe <austine.udhe@kodehauz.com>
2024-02-29 16:37:05 +0100

    ch: optimize workflow

--- src/tests/components/App.spec.jsx
+++ src/tests/components/App.spec.jsx
@@ -4,7 +4,7 @@ import Home from '../../pages/Home'
 
 describe('a simple test coverage', () => {
   it('should check if truthy', () => {
-    expect(1).toBeTruthy()
+    expect(0).toBeTruthy()
   })
 
   it('should display a text on the screen', () => {
